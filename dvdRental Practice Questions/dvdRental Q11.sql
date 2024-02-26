796def320b8ffc3adba711846096506a66ab4aed
AbdulAzeez Adesanya <adesanyaabdulazeez@yahoo.com>
2024-02-26 17:57:39 +0100

    Delete dvdRental Practice Questions/dvdRental Q11.sql

--- dvdRental Practice Questions/dvdRental Q11.sql
+++
@@ -1,7 +0,0 @@
---Q11. Show the profit of Store 1 and 2?
-
-SELECT i.store_id, SUM(p.amount) Total_amount
-FROM payment p
-JOIN rental r ON p.rental_id = r.rental_id
-JOIN inventory i ON r.inventory_id = i.inventory_id
-GROUP BY 1
