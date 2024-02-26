5c7c7fa15ec1ae6dc4e520337827850e062fb2b4
AbdulAzeez Adesanya <adesanyaabdulazeez@yahoo.com>
2024-02-26 17:58:46 +0100

    Delete dvdRental Practice Questions/dvdRental Q3.sql

--- dvdRental Practice Questions/dvdRental Q3.sql
+++
@@ -1,6 +0,0 @@
--- Q3. Are there customers that share the same address?
-
-SELECT t1.first_name, t2.last_name
-FROM customer t1, customer t2 
-WHERE t1.customer_id <> t2.customer_id
-AND t1.address_id = t2.address_id
