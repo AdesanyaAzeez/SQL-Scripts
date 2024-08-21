fee82c2650d51c3caf7db1269065d2262e6cb64c
AbdulAzeez Adesanya <adesanyaabdulazeez@yahoo.com>
2024-08-21 12:40:09 +0100

    Update update_product_record.sql

--- Stored Procedures1 - Sales & Inventory Management/update_product_record.sql
+++ Stored Procedures1 - Sales & Inventory Management/update_product_record.sql
@@ -1,5 +1,5 @@
--- create a stored procedure for adding a new product or update the stock unit of an existing one
-CREATE OR REPLACE PROCEDURE pr_product_update(pm_product_id INT, pm_product_name VARCHAR(200), pm_price FLOAT, pm_quantity INT)
+-- create a stored procedure for adding a new product/updating the product details of an existing one and keeping a record of all events
+CREATE OR REPLACE PROCEDURE pr_product_update(pm_product_id INT, pm_product_name VARCHAR(200), pm_price FLOAT, pm_quantity INT, pm_narration VARCHAR(100))
 LANGUAGE plpgsql 
 AS $$
 DECLARE
@@ -10,6 +10,8 @@ DECLARE
 	vr_quantity_sold INT DEFAULT 0;
 	vr_new_quantity INT;
 	vr_old_quantity INT;
+	vr_old_price FLOAT;
+	vr_new_price FLOAT;
 	vr_id_exist BOOLEAN;
 	vr_name_exist BOOLEAN;
 	
@@ -44,31 +46,32 @@ BEGIN
 	
 	-- If parameter ID is on the table but parameter NAME is not/does not tally
 	IF vr_id_exist THEN
-		
+		-- show error message 
 		RAISE NOTICE 'Id "%" already assigned to product "%". Use a different Product Id/Correct Product Name', 
 			pm_product_id, vr_name_check;
 	
 	-- If parameter NAME is on the table but parameter ID is not/does not tally
 	ELSIF vr_name_exist THEN 
-		
+		-- show error message
 		RAISE NOTICE 'Product "%" already exist with different Id "%". Use correct Id/Change Product Name', 
 			pm_product_name, vr_id_check;
 	
-	-- If both parameter ID and NAME does not exist
+	-- Else If both parameter ID and NAME does not exist
 	ELSIF vr_product_id IS NULL AND vr_product_name IS NULL THEN
 		
 		-- Insert new record into the product table
 		INSERT INTO products (product_id, product_name, price, quantity_sold, quantity_available)
 		VALUES
 		(pm_product_id, pm_product_name, pm_price, vr_quantity_sold, pm_quantity);
-		
-		RAISE NOTICE '% unit(s) of "%" added to record', pm_quantity, pm_product_name;
+
+		-- show details of new product added to record
+		RAISE NOTICE '% unit(s) of "%" @ % added to record', pm_quantity, pm_product_name, pm_price;
 	
-	-- If parameter ID and NAME matches in the products table
+	-- Otherwise If parameter ID and NAME matches in the products table
 	ELSE
 		-- get quantity available before record update
-		SELECT quantity_available
-			INTO vr_old_quantity
+		SELECT quantity_available, price
+			INTO vr_old_quantity, vr_old_price
 		FROM products
 		WHERE product_name = pm_product_name
 			AND product_id = pm_product_id;
@@ -81,26 +84,55 @@ BEGIN
 			AND product_id = pm_product_id;
 		
 		-- get quantity available after record update
-		SELECT quantity_available
-			INTO vr_new_quantity
+		SELECT quantity_available, price
+			INTO vr_new_quantity, vr_new_price
 		FROM products
 		WHERE product_name = pm_product_name
 			AND product_id = pm_product_id;
-		
-		RAISE NOTICE '% unit(s) added. Product "%" stock unit updated from % to %', 
-			pm_quantity, pm_product_name, vr_old_quantity, vr_new_quantity;
+
+		-- if price remains same
+		IF vr_old_price = vr_new_price THEN
+			-- show unit update details
+			RAISE NOTICE 'Product "%" Details Updated. Unit updated from % to %', 
+				pm_product_name, vr_old_quantity, vr_new_quantity;
+				
+		-- if quantity remains same	
+		ELSIF vr_old_quantity = vr_new_quantity THEN
+			-- show price update details
+			RAISE NOTICE 'Product "%" Details Updated. Price updated from % to %',
+				pm_product_name, vr_old_price, vr_new_price;
+
+		-- if both price and quantity changes		
+		ELSE
+			RAISE NOTICE 'Product "%" Details Updated. Price updated from % to %, and units from % to %',
+				pm_product_name, vr_old_price, vr_new_price, vr_old_quantity, vr_new_quantity;
+		END IF;
 
 	END IF;
-	
+
+	-- if there's no mismatch in product id/name 
+	IF NOT (vr_id_exist OR vr_name_exist) THEN
+		-- insert update details into the PRODUCT HISTORY table
+		INSERT INTO product_history(product_id, price, unit_added, created_at, narration)
+		VALUES
+		(pm_product_id, pm_price, pm_quantity, NOW(), pm_narration);
+		-- show success message
+		RAISE NOTICE 'Record history update successful';
+	END IF;
+
 END;
 $$;
 
 
--- execute procedure with existing product name and product_id
-CALL pr_product_update(1001, 'Wooden Stool', 450000, 5);
+
+-- execute procedure with existing product name and product_id, updating product unit
+CALL pr_product_update(1001, 'Wooden Stool', 450000, 5, 'Update product quantity');
+
+-- execute procedure with existing product name and product_id, updating price
+CALL pr_product_update(1001, 'Wooden Stool', 450500, 0, 'Update product price');
 
 -- execute procedure with new product name and product id
-CALL pr_product_update(1004, 'Plastic Chair', 98500, 10);
+CALL pr_product_update(1004, 'Plastic Chair', 98500, 10, 'New product added');
 
 -- execute procedure with existing product name but wrong product_id
 CALL pr_product_update(1006, 'Laptop Stand', 98500, 10);
@@ -108,6 +140,10 @@ CALL pr_product_update(1006, 'Laptop Stand', 98500, 10);
 -- execute procedure with existing product id but wrong product name
 CALL pr_product_update(1006, 'Laptop Stand', 98500, 10);
 
+
 -- see product table to check for changes
 SELECT * FROM products ;
+
+-- see product history table for record history
+SELECT * FROM product_history ;
 
